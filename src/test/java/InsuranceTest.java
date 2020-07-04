a2e3ec995a589417b0913a23623cb79ecfaddb46
Mikhail Tikhonov <46346390+MikhailTikhonovA@users.noreply.github.com>
2020-07-04 11:44:13 +0300

    Initial commit

---
+++ src/test/java/InsuranceTest.java
@@ -0,0 +1,22 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class InsuranceTest {
+    WebDriver driver;
+    String baseUrl;
+    @Before
+    public void beforeTest(){
+
+    }
+    @Test
+    public void testInsurance(){
+
+    }
+    @After
+    public void afterTest(){
+
+    }
+
+}
